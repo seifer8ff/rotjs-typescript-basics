70177a9043ebcaef07f52cff7c0c392eb5e7cd6a
Brady Mauermann-Peterson <brady.bcmp@gmail.com>
2024-02-20 09:22:54 -0800

    Feat: interactivity (#2)
    * feat: initial UI
    
    Bring in shoelace for a lightweight web component library.
    Build out first UI screen- gameplay HUD with links to "pause" menu.
    Hook up pause button to game pause.
    Add TimeManager to handle everything turn/scheduling/time related.
    Hook up current time to TimeControl component.
    
    * feat: tabbed gameplay UI
    
    Add a tabbed gameplay UI- select section with button at top, and clickable options are shown in list below.
    Add time control section with current date, pause button, and game speed scale.
    Hook up speed scale to timemanager.
    
    * fix: warn about backdropFilter
    
    * Move rendering to its own loop
    
    Have one loop for rendering and one for game logic.
    Run each loop at different intervals, the game loop at a slower interval.

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
 import { Game } from "./game";
 import "./style.css";
+import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path.js";
+
+// Set shoelace base path
+// this path works on both prod and dev
+// during build process, assets are copied from node module into root and dist folder
+setBasePath("/shoelace");
 
 document.body.onload = async () => {
   var game = new Game();
