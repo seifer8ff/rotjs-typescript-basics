edc7e18e5eac2782708d76566bb13b547a1b6e50
Brady Mauermann-Peterson <brady.bcmp@gmail.com>
2024-02-22 09:16:11 -0800

    build fixes

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path.j
 // Set shoelace base path
 // this path works on both prod and dev
 // during build process, assets are copied from node module into root and dist folder
-setBasePath("public/shoelace");
+setBasePath("./public/shoelace");
 
 document.body.onload = async () => {
   var game = new Game();
