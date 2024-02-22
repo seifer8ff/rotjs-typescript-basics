30e5c85ef4142bbc335facc21a4e0fee7b05eec4
Brady Mauermann-Peterson <brady.bcmp@gmail.com>
2024-02-22 09:50:12 -0800

    test

--- webpack.common.js
+++ webpack.common.js
@@ -81,7 +81,7 @@ module.exports = {
                 <meta http-equiv="X-UA-Compatible" content="IE=edge">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
                 <meta name="description" content="Your game description goes here.">     
-                <link href="./shoelace/themes/dark.css" rel="stylesheet" />       
+                <link href="shoelace/themes/dark.css" rel="stylesheet" />       
             </head>
             <body>
                 <div id="uiContainer">
